12222876d33296cc05f76c43a496e6232e9cb06e
vishv0407 <vishvboda0407@gmail.com>
2025-01-05 23:13:59 +0530

    formetted app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
 import { WebSocketProvider } from "./context/WebSocketContext";
 import Navbar from "./components/Navbar";
 import AdminNavbar from "./components/AdminNavbar";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import Admin from "./pages/Admin";
 import TeamPage from "./pages/TeamPage";
-import { Toaster } from 'react-hot-toast';
-import Footer from './components/Footer';
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 import Scoreboard from "./pages/Scoreboard";
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminLogs from "./pages/AdminLogs";
@@ -15,7 +20,7 @@ import RequireAuth from "./components/RequireAuth";
 
 function AppContent() {
   const location = useLocation();
-  const isAdminRoute = location.pathname.startsWith('/adminpage');
+  const isAdminRoute = location.pathname.startsWith("/adminpage");
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
@@ -25,33 +30,33 @@ function AppContent() {
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/scoreboard" element={<Scoreboard />} />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="/teams/:teamName" element={<TeamPage />} />
-          <Route path="/adminpage" element={<Navigate to="/adminpage/dashboard" />} />
-          <Route 
-            path="/adminpage/dashboard" 
+          <Route path="/teams/:teamName" element={<TeamPage />} />
+          <Route
+            path="/adminpage"
+            element={<Navigate to="/adminpage/dashboard" />}
+          />
+          <Route
+            path="/adminpage/dashboard"
             element={
               <RequireAuth>
                 <AdminDashboard />
               </RequireAuth>
-            } 
+            }
           />
-          <Route 
-            path="/adminpage/logs" 
+          <Route
+            path="/adminpage/logs"
             element={
               <RequireAuth>
                 <AdminLogs />
               </RequireAuth>
-            } 
+            }
           />
         </Routes>
       </div>
-    
+
       <Footer />
-          <Toaster 
-            position="top-right"
-          />
-        </div>
+      <Toaster position="top-right" />
+    </div>
   );
 }
 
